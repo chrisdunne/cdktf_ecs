5538718a3fdfe1f39c964cdcb4676091cb9ede0c
chris <contact@chrisdunne.net>
2020-07-18 02:30:59 +0100

    define instance in eu-west-2

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import { AwsProvider, Instance } from './.gen/providers/aws';
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-    // define resources here
+    new AwsProvider(this, 'aws', {
+      region: 'eu-west-2'
+    });
 
+    new Instance(this, 'cdktf_instance', {
+      ami: "ami-0376206bb575c76dd",
+      instanceType: "t2.micro"
+    });
   }
 }
 
